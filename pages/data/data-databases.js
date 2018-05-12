6ddadb646b2f882a8ef49e58866259bd7e14ac3f
mnmnmssd <luyifanshihaoren@163.com>
2018-05-12 23:14:00 +0800

    v1.2
    初步代码

---
+++ pages/data/data-databases.js
@@ -0,0 +1,28 @@
+var local_database = [{
+  avatar:"avatar/1.png",
+  date:"于2018年5月12日发布",
+  post:"XXX我爱你！！",
+  contouned:"1024"
+},
+{
+  avatar: "avatar/2.png",
+  date: "于2018年5月10日发布",
+  post: "XXX！！",
+  contouned: "2048"
+},
+{
+  avatar: "avatar/3.png",
+  date: "于2018年5月12日发布",
+  post: "爱你一辈子！！",
+  contouned: "4096"
+},
+{
+  avatar: "avatar/4.png",
+  date: "于2018年5月11日发布",
+  post: "贼吉儿爱你",
+  contouned: "512"
+}
+]
+module.exports = {
+  postlist: local_database
+}
